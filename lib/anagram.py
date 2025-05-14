d49b44a55807e805591706e8ac322976b8d0def1
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-05-14 17:49:46 +0300

    Implement Anagram class with match() method to detect anagrams and pass all tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+    
+    def match(self, potential_anagrams):
+        matches = []
+        
+        for possible in potential_anagrams:
+            if sorted(possible) == self.sorted_word:
+                matches.append(possible)
+        
+        return matches
